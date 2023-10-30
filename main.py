16b6f1ab464ab4fd37c9bf873e7380426f532106
Rmaon <ramoncamineroarroyo@gmail.com>
2023-10-30 19:50:45 +0100

    Cambio del entorno virtual a 3.10 para poder importar el paquete de discord

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import bs4
+import requests
+import discord
+from discord.ext import commands
+import datetime
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+urlLanzamientos = "https://vandal.elespanol.com/lanzamientos/0/videojuegos"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+token = 'MTE2NzQ2MjY4OTkyMTA1Njg4MA.GzUeAM.4NHKRuYKaP3weFtM3ibz29pbaN6LsoVnKW03CA'
